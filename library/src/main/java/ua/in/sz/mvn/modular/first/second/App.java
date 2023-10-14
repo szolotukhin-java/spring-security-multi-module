cfb21514db5cc068551f72f817bb1b95188dbfca
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-10-14 21:01:23 +0300

    init

--- library/src/main/java/ua/in/sz/mvn/modular/first/second/App.java
+++
@@ -1,9 +0,0 @@
-package ua.in.sz.mvn.modular.first.second;
-
-public class App {
-    public static String GREETING = "Hello World!";
-
-    public static void main(String[] args) {
-        System.out.println(GREETING);
-    }
-}
